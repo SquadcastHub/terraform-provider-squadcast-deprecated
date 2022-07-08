d0e42556f8f3967257160d5d711fa40a2c568cc2
Dheeraj Kumar <89898838+dheeraj-squadcast@users.noreply.github.com>
2022-07-08 16:23:00 +0530

    fix test

--- internal/api/teams.go
+++ internal/api/teams.go
@@ -11,12 +11,12 @@ import (
 )
 
 type Team struct {
-	ID          string        `json:"id" tf:"id"`
-	Name        string        `json:"name" tf:"name"`
-	Description string        `json:"description" tf:"description"`
-	Default     bool          `json:"default" tf:"default"`
-	Members     []*TeamMember `json:"members" tf:"-"`
-	Roles       []*TeamRole   `json:"roles" tf:"-"`
+	ID          string            `json:"id" tf:"id"`
+	Name        string            `json:"name" tf:"name"`
+	Description string            `json:"description" tf:"description"`
+	Default     bool              `json:"default" tf:"default"`
+	Members     []*DataTeamMember `json:"members" tf:"-"`
+	Roles       []*TeamRole       `json:"roles" tf:"-"`
 }
 
 func (t *Team) Encode() (tf.M, error) {
@@ -38,6 +38,15 @@ func (t *Team) Encode() (tf.M, error) {
 	m["roles"] = roles
 
 	return m, nil
+}
+
+type DataTeamMember struct {
+	UserID  string   `json:"user_id" tf:"user_id"`
+	RoleIDs []string `json:"role_ids" tf:"role_ids"`
+}
+
+func (tm *DataTeamMember) Encode() (tf.M, error) {
+	return tf.Encode(tm)
 }
 
 type TeamMember struct {
